f06206f0a5afeabd05289944e13f9cc12aa7aca3
raythurman2386 <raymondthurman5@gmail.com>
2023-02-02 08:10:02 -0600

    added test cases for random strings in the data

--- tests/data_test.py
+++ tests/data_test.py
@@ -20,37 +20,50 @@ class TestDataFuctions(unittest.TestCase):
     
     def test_clean_phone(self):
         expected = clean_phone("(812)555-5647")
-        self.assertTrue(expected, "8125555647")
+        commas = clean_phone("812,555,5555")
+        self.assertEqual(expected, "8125555647")
+        self.assertEqual(commas, "8125555555")
 
     
     def test_correct_race(self):
         expected = correct_race("American Indian/Alaskan Native")
+        random_string = correct_race("Some Random String")
         self.assertTrue(expected, options['race']['a'])
+        self.assertTrue(random_string, options['race']['d'])
 
 
     def test_correct_ethnicity(self):
         expected = correct_ethnicity('a. hispanic')
+        random_string = correct_ethnicity("Some Random String")
         self.assertTrue(expected, options['ethnicity']['a'])
+        self.assertTrue(random_string, options['ethnicity']['c'])
 
     
     def test_correct_eng_prof(self):
         expected = correct_eng_prof('a. Household is Limited English Proficient')
+        random_string = correct_eng_prof("Some Random String")
         self.assertTrue(expected, options['englishProficiency']['a'])
-
+        self.assertTrue(random_string, options['englishProficiency']['c'])
 
     def test_correct_case_type(self):
         expected = correct_case_type('Home Purchase')
+        random_string = correct_case_type("Some Random String")
         self.assertTrue(expected, options['caseType']['a'])
+        self.assertTrue(random_string, options['caseType']['a'])
 
 
     def test_correct_rural(self):
         expected = correct_rural('Household lives in a rural area')
+        random_string = correct_rural("Some Random String")
         self.assertTrue(expected, options['RuralAreaStatus']['a'])
+        self.assertTrue(random_string, options['RuralAreaStatus']['c'])
 
 
     def test_correct_household(self):
         expected = correct_household('Female-Single Parent')
+        random_string = correct_household("Some Random String")
         self.assertTrue(expected, options['HouseholdType']['b'])
+        self.assertTrue(random_string, options['HouseholdType']['g'])
 
 if __name__ == 'main':
     unittest.main()
